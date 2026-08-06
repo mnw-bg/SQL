00036cdd23c54a69a48578180642789b0599f67d
warayut <lastine100720@gmail.com>
2026-08-06 15:06:14 +0300

    kertaus

---
+++ kertaus/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>palautesovellus</title>
+</head>
+<body>
+    <marquee behavior="scroll" direction="up" scrollamount="400"><h1>tervetuloa</h1></marquee>
+    <marquee behavior="scroll" direction="up" scrollamount="90">Tämä on minun ensimmäinen nettisivuni.</marquee>
+    <?php
+    echo "<marquee behavior=\"scroll\" direction=\"up\" scrollamount=\"10\">Tämä on minun ensimmäinen PHP-sivuni.</marquee>";
+    ?>
+
+</body>
+</html>
